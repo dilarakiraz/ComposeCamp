1e0e563468137537a4d8fd43b26b81bb43b80312
dilarakiraz <dilarakiraz12345@gmail.com>
2024-09-11 14:40:14 +0300

    multiple selection lazy column exercise

---
+++ app/src/main/java/com/example/composecamp/Unit7_MultiSelectLazyColumn/ListItem.kt
@@ -0,0 +1,9 @@
+package com.example.composecamp.Unit7_MultiSelectLazyColumn
+
+/**
+ * Created by Dilara Kiraz on 11.09.2024.
+ */
+data class ListItem(
+    val title: String,
+    val isSelected: Boolean
+)
